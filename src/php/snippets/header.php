eebf86d18c7b03d99f35d7b5d703caa688ac2d67
xnocken <minecraftmarc746@gmail.com>
2020-01-29 22:27:01 +0100

    chore(init): initial commit

---
+++ src/php/snippets/header.php
@@ -0,0 +1,39 @@
+<header>
+  <div class="header">
+    <div class="header--navigation">
+      <div class="navgation">
+        <div class="navigation--entry">
+          <a href="/dahinten.html">dahinten</a>
+        </div>
+        <div class="navigation--entry">
+          <a href="/dahinten.html">dahinten</a>
+        </div>
+        <div class="navigation--entry">
+          <a href="/dahinten.html">dahinten</a>
+        </div>
+        <div class="navigation--entry">
+          <a href="/dahinten.html">dahinten</a>
+        </div>
+        <div class="navigation--entry">
+          <a href="/dahinten.html">dahinten</a>
+        </div>
+        <div class="navigation--entry">
+          <a href="/dahinten.html">dahinten</a>
+        </div>
+        <div class="navigation--entry">
+          <a href="/dahinten.html">dahinten</a>
+        </div>
+      </div>
+    </div>
+    <div class="header--user">
+      <div class="user">
+        <div class="user--name">
+          xNocken
+        </div>
+        <div class="user--image">
+          <img src="http://placekitten.com/50/50">
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'web/dist'),
+    filename: 'app.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+};
