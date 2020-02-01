f5c13f0d5f231313d179c00f5ad8fdb491a3445d
xnocken <minecraftmarc746@gmail.com>
2020-02-01 01:24:03 +0100

    fix(navigations): fixes navigations throwing error on other pages

--- src/js/navigations.js
+++ src/js/navigations.js
@@ -4,15 +4,20 @@ export default () => {
   const toggleActiveButtons = document.getElementsByClassName('active-button');
   const addNavigationButton = document.getElementById('add-navigation-button');
 
-  Array.from(toggleActiveButtons).forEach((button) => {
-    button.addEventListener('click', (event) => {
-      console.log(event.target.parentNode.parentNode.getAttribute('data-id'));
+  if (toggleActiveButtons) {
+    Array.from(toggleActiveButtons).forEach((button) => {
+      button.addEventListener('click', (event) => {
+        console.log(event.target.parentNode.parentNode.getAttribute('data-id'));
+      });
     });
-  });
+  }
 
-  addNavigationButton.addEventListener('click', () => {
-    request('/admin/api/addNavigation', { name: 'Test', path: '/test/', active: 1 }, (response) => {
-      console.log(response);
+  if (addNavigationButton) {
+
+    addNavigationButton.addEventListener('click', () => {
+      request('/admin/api/addNavigation', { name: 'Test', path: '/test/', active: 1 }, (response) => {
+        console.log(response);
+      });
     });
-  });
+  }
 };
